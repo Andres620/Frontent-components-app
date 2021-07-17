7fe75258e0465e9e543a8ec88ef91456d4a77a97
Andres620 <andres.1059814779@gmail.com>
2021-07-17 18:43:57 -0500

    Cambio de nombre del proyecto

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/PCcomponentsApp'),
+      dir: require('path').join(__dirname, './coverage/Frontend-components-app'),
       subdir: '.',
       reporters: [
         { type: 'html' },
