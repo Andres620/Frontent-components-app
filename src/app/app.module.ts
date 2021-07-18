1c67e42ed496d77506aea9180a7d2e42fae18ba9
Andres620 <andres.1059814779@gmail.com>
2021-07-18 17:38:11 -0500

    Se a la carpeta assets la carpeta js y se modifico el navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './template/header/header.component';
+import { NavbarComponent } from './template/header/navbar.component';
 import { FooterComponent } from './template/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
+    NavbarComponent,
     FooterComponent
   ],
   imports: [
