09bbba4f417a3dd3fdbced9a84f683b43821e459
Andres620 <andres.1059814779@gmail.com>
2021-07-20 21:02:08 -0500

    Se creo el hero de la aplicacion

--- src/assets/js/myScripts.js
+++ src/assets/js/myScripts.js
@@ -1,5 +1,21 @@
-
+//Navbar
   document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems);
   });
+
+  //Hero carousel
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.carousel');
+    var instance = M.Carousel.init(elems, {
+      fullWidth: true,
+      indicators: true
+    });
+  });
+
+
+  
+
+
+  
