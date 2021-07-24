014c4ad35c40580998fb794a06206763346b151f
Andres620 <andres.1059814779@gmail.com>
2021-07-24 17:29:37 -0500

    Animación del carousel

--- src/assets/js/myScripts.js
+++ src/assets/js/myScripts.js
@@ -1,21 +1,33 @@
 //Navbar
-  document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems);
+document.addEventListener('DOMContentLoaded', function () {
+  var elems = document.querySelectorAll('.sidenav');
+  var instances = M.Sidenav.init(elems);
+});
+
+//Hero carousel
+
+document.addEventListener('DOMContentLoaded', function () {
+  var elems = document.querySelectorAll('.carousel');
+  var instance = M.Carousel.init(elems, {
+    fullWidth: true,
+    indicators: true,
+    autoplay: true
   });
 
-  //Hero carousel
+  let indicatorItems = document.querySelectorAll('.carousel .indicator-item'),
+    slideTime = 6000,
+    activeClass = "active";
 
-  document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.carousel');
-    var instance = M.Carousel.init(elems, {
-      fullWidth: true,
-      indicators: true
+  setInterval(() => {
+    indicatorItems.forEach(el => {
+      if (el.classList.contains(activeClass)) {
+        sib = el.nextElementSibling;
+        if (sib == null) {
+          indicatorItems[0].click();
+        } else {
+          sib.click()
+        }
+      }
     });
-  });
-
-
-  
-
-
-  
+  }, slideTime);
+});
